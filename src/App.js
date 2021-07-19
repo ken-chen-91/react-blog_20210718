a69d024972d4d537fc1431d63dd5d07cc6e0ddf3
kenchen <a330120231@gmail.com>
2021-07-19 00:59:40 -0700

    finish frontend UI design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Topbar from "./components/topbar/Topbar";
+import Home from "./pages/home/Home";
+import Single from "./pages/single/Single";
+import Write from "./pages/Write/Write";
+import Settings from "./pages/settings/Settings";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const currentUser = false;
+
+    return (
+        <Router>
+            <Topbar />
+            <Switch>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+                <Route path="/posts">
+                    <Home />
+                </Route>
+                <Route path="/register">
+                    {currentUser ? <Home /> : <Register />}
+                </Route>
+                <Route path="/login">{currentUser ? <Home /> : <Login />}</Route>
+                <Route path="/post/:id">
+                    <Single />
+                </Route>
+                <Route path="/write">{currentUser ? <Write /> : <Login />}</Route>
+                <Route path="/settings">
+                    {currentUser ? <Settings /> : <Login />}
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
